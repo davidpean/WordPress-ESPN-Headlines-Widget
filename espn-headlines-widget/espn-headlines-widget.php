e8206dab41e1a41b09e658e6721a7b4ba402a35a
David Pean <david.pean@gmail.com>
2012-04-28 10:33:09 -0400

    formatting got wonky fromusing multiple IDEs

--- espn-headlines-widget/espn-headlines-widget.php
+++ espn-headlines-widget/espn-headlines-widget.php
@@ -9,8 +9,8 @@ Author URI: http://www.twitter.com/davidpean
    
 class ESPN_Headlines_Widget extends WP_Widget{
 
-    var $cacheKey = "espn_headlines";
-    var $apiBaseURL = "http://api.espn.com/v1/";
+	var $cacheKey = "espn_headlines";
+	var $apiBaseURL = "http://api.espn.com/v1/";
         
 	/**
 	 * Register widget with WordPress.
@@ -26,19 +26,19 @@ class ESPN_Headlines_Widget extends WP_Widget{
 		);
 	}
 
-    /**
-    * Outputs the content of the widget.
-    *
-    * @args The array of form elements
-    * @instance
-    */
+	/**
+ 	* Outputs the content of the widget.
+ 	*
+ 	* @args The array of form elements
+  	* @instance
+ 	*/
 	function widget( $args, $instance ) {
 	
 		extract( $args, EXTR_SKIP );
 		
 		echo $before_widget;
     
-        $widget_title = empty($instance['widget_title']) ? '' : apply_filters('widget_title', $instance['widget_title']);
+        	$widget_title = empty($instance['widget_title']) ? '' : apply_filters('widget_title', $instance['widget_title']);
 
 		// Display the widget
 		include( plugin_dir_path(__FILE__) . '/views/widget.php' );
@@ -47,51 +47,51 @@ class ESPN_Headlines_Widget extends WP_Widget{
 		
 	} // end widget
 
-    /**
-    * Processes the widget's options to be saved.
-    *
-    * @new_instance The new instance of values to be generated via the update.
-    * @old_instance The previous instance of values before the update.
-    */
+ 	/**
+  	* Processes the widget's options to be saved.
+  	*
+  	* @new_instance The new instance of values to be generated via the update.
+  	* @old_instance The previous instance of values before the update.
+  	*/
 	function update( $new_instance, $old_instance ) {
 		
 		$instance = $old_instance;
 		
-        delete_transient( $this->cacheKey );
+        	delete_transient( $this->cacheKey );
                 
 		$numberToShow = (int) $new_instance['widget_headlines_number'];
 		if ( numberToShow == 0 || ! $numberToShow = absint( $numberToShow ) ){
  			$number = 10;
  		}
  		
-        $instance['widget_title'] = strip_tags($new_instance['widget_title']);
-        $instance['widget_headlines_number'] = $numberToShow;
-        $instance['widget_espn_apikey'] = strip_tags($new_instance['widget_espn_apikey']);
-        $instance['widget_headlines_type'] = $new_instance['widget_headlines_type'];
-        $instance['widget_headlines_filter'] = $new_instance['widget_headlines_filter'];
-        $instance['widget_show_description'] = isset($new_instance['widget_show_description']);
+        	$instance['widget_title'] = strip_tags($new_instance['widget_title']);
+        	$instance['widget_headlines_number'] = $numberToShow;
+        	$instance['widget_espn_apikey'] = strip_tags($new_instance['widget_espn_apikey']);
+        	$instance['widget_headlines_type'] = $new_instance['widget_headlines_type'];
+        	$instance['widget_headlines_filter'] = $new_instance['widget_headlines_filter'];
+        	$instance['widget_show_description'] = isset($new_instance['widget_show_description']);
                 
 		return $instance;
 		
 	} // end widget
 
-    /**
-    * Generates the administration form for the widget.
-    *
-    * @instance The array of keys and values for the widget.
-    */
+  	/**
+  	* Generates the administration form for the widget.
+  	*
+  	* @instance The array of keys and values for the widget.
+  	*/
 	function form( $instance ) {
 	
 		$instance = wp_parse_args(
 			(array) $instance,
 			array(
 				'widget_title'                  =>      '',
-                'widget_headlines_number'	=>	5
+                		'widget_headlines_number'	=>	5
 			)
 		);
 	
 		// Display the admin form
-        include( plugin_dir_path(__FILE__) . '/views/admin.php' );
+        	include( plugin_dir_path(__FILE__) . '/views/admin.php' );
 		
 	} // end form
 
